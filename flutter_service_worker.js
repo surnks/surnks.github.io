92a861c6fac7a946c8ff8711ca8669fe2291c012
홍승권 <seungkwon@hongseung-gwon-ui-MacBookPro.local>
2021-07-20 16:44:40 +0900

    rebulid canvaskit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "bfd703aa85807d5443f8c85c01dd355d",
-"index.html": "a71cde881059f464649969b14313abd2",
-"/": "a71cde881059f464649969b14313abd2",
-"main.dart.js": "78ff5171ce6f596dc4bf21108294ad90",
+"index.html": "73b9c956cbf0b21108f22f5f660b1718",
+"/": "73b9c956cbf0b21108f22f5f660b1718",
+"main.dart.js": "bbdce2fb5e84a787ed21d4aeaaa18f0c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
